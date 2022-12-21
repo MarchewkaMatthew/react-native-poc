f02f14d217d3300e8aa8eb9a8aaf37c3107e27a1
Mateusz <marchewka.matthew@gmail.com>
2022-12-21 11:35:30 +0100

    stack navigation works

--- App.styles.tsx
+++ App.styles.tsx
@@ -1,25 +1,34 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #000;
   padding: 16px
+`;
+
+export const ScreenContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  background-color: #000;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid red;
 `;
 
 export const Inner = styled.View`
   width: 100%;
   height: 100%;
   border: 1px solid white;
-  align-items: center;
-  justify-content: center;
-  border-color: white;
-  border-width: 1px;
 `
 
-export const Box = styled.View`
+interface BoxProps {
+  bgColor: string;
+}
+
+export const Box = styled.View<BoxProps>`
   width: 50%;
   height: 24px;
-  background-color: red;
+  background-color: ${p => p.bgColor};
   margin-top: 24px;
 `;
 
--- App.tsx
+++ App.tsx
@@ -1,15 +1,52 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { Box, Container, Inner, Text } from './App.styles';
+import { Box, Container, Inner, ScreenContainer, Text } from './App.styles';
+// import { createDrawerNavigator, DrawerScreenProps, DrawerContentComponentProps } from '@react-navigation/drawer';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button } from 'react-native';
+
+type MainNavigationParamList = {
+  Welcome: undefined;
+  Settings: undefined;
+};
+
+const Stack = createNativeStackNavigator<MainNavigationParamList>();
+
+const WelcomeScreen = ({navigation}: NativeStackScreenProps<MainNavigationParamList>) => {
+  return (
+    <ScreenContainer>
+      <Text>JEBAĆ PIS</Text>
+      <Box bgColor="green" />
+      <Button title='Go to Settings' onPress={() => navigation.navigate('Settings')} />
+    </ScreenContainer>
+  )
+}
+
+const SettingsScreen = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<MainNavigationParamList>>();
+
+  return (
+    <ScreenContainer>
+      <Text>I KONFEDERACJE</Text>
+      <Box bgColor="red" />
+      <Button title='Go to Welcome' onPress={() => navigation.navigate("Welcome")} />
+    </ScreenContainer>
+  )
+}
 
 export default function App() {
   return (
     <Container>
       <Inner>
-        <Text>JEBAĆ PIS</Text>
-        <Box />
-        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Welcome'>
+            <Stack.Screen name='Welcome' component={WelcomeScreen} />
+            <Stack.Screen name='Settings' component={SettingsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </Inner>
+      <StatusBar style="auto" />
     </Container>
   );
 }
